b0f485d51c02279f7430604ee567b5157ce17b06
EuromuroOdoo <m.iakubovich@euromuro.eu>
2026-07-23 17:58:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const p = new URLSearchParams(window.location.search);
 
-const code = p.get("code");
-const name = decodeURIComponent(p.get("name"));
-const url = p.get("qr");
-const qty = parseFloat(p.get("qty"));
-const pallet = parseFloat(p.get("pallet"));
-const mo = decodeURIComponent(p.get("mo"));
+const code = p.get("code") || "";
+const name = decodeURIComponent(p.get("name") || "");
+const url = p.get("qr") || "";
+const qty = parseFloat(p.get("qty") || 0);
+const pallet = parseFloat(p.get("pallet") || 1);
+const mo = decodeURIComponent(p.get("mo") || "");
+const barcode = p.get("barcode") || "";
 
 const labels = document.getElementById("labels");
 
@@ -21,6 +22,7 @@ for (let i = 0; i < pallets; i++) {
     const div = document.createElement("div");
     div.className = "label";
 
+    // QR
     const qr = document.createElement("div");
     div.appendChild(qr);
 
@@ -30,19 +32,25 @@ for (let i = 0; i < pallets; i++) {
         type: "svg",
         data: url,
         image: "logo.png",
+
         dotsOptions: {
             type: "rounded"
         },
+
         cornersSquareOptions: {
             type: "extra-rounded"
         },
+
         cornersDotOptions: {
             type: "dot"
         },
+
         imageOptions: {
             margin: 6,
-            imageSize: 0.28
+            imageSize: 0.28,
+            crossOrigin: "anonymous"
         },
+
         qrOptions: {
             errorCorrectionLevel: "H"
         }
@@ -50,21 +58,48 @@ for (let i = 0; i < pallets; i++) {
 
     qrCode.append(qr);
 
+    // Product code
     const c = document.createElement("div");
     c.className = "code";
     c.innerText = code;
     div.appendChild(c);
 
+    // Product name
     const n = document.createElement("div");
     n.className = "name";
     n.innerText = name;
     div.appendChild(n);
 
+    // Quantity
     const q = document.createElement("div");
     q.className = "qty";
     q.innerText = current + " units";
     div.appendChild(q);
 
+    // Barcode
+    if (barcode) {
+
+        const svg = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "svg"
+        );
+
+        svg.classList.add("barcode");
+
+        div.appendChild(svg);
+
+        JsBarcode(svg, barcode, {
+            format: "CODE128",
+            width: 2,
+            height: 42,
+            displayValue: true,
+            fontSize: 15,
+            margin: 0
+        });
+
+    }
+
+    // Manufacturing Order
     const m = document.createElement("div");
     m.className = "mo";
     m.innerText = mo;
@@ -74,5 +109,7 @@ for (let i = 0; i < pallets; i++) {
 }
 
 window.onload = () => {
-    setTimeout(() => window.print(), 700);
+    setTimeout(() => {
+        window.print();
+    }, 700);
 };
