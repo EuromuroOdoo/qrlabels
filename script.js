6ad1facfb7d3073d1cfd70e3b4c5335f345d7739
EuromuroOdoo <m.iakubovich@euromuro.eu>
2026-07-23 18:11:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ const qty = parseFloat(p.get("qty") || 0);
 const pallet = parseFloat(p.get("pallet") || 1);
 const mo = decodeURIComponent(p.get("mo") || "");
 const barcode = p.get("barcode") || "";
-console.log("Barcode:", barcode);
+
 const labels = document.getElementById("labels");
 
 const pallets = Math.ceil(qty / pallet);
+
+// Собираем промисы завершения рендера QR
+const qrPromises = [];
 
 for (let i = 0; i < pallets; i++) {
 
@@ -22,14 +25,13 @@ for (let i = 0; i < pallets; i++) {
     const div = document.createElement("div");
     div.className = "label";
 
-    // QR
-    const qr = document.createElement("div");
-    div.appendChild(qr);
+    const qrContainer = document.createElement("div");
+    div.appendChild(qrContainer);
 
     const qrCode = new QRCodeStyling({
         width: 220,
         height: 220,
-        type: "svg",
+        type: "canvas",
         data: url,
         image: "logo.png",
 
@@ -56,50 +58,31 @@ for (let i = 0; i < pallets; i++) {
         }
     });
 
-    qrCode.append(qr);
+    // append() возвращает Promise
+    qrPromises.push(qrCode.append(qrContainer));
 
-    // Product code
     const c = document.createElement("div");
     c.className = "code";
     c.innerText = code;
     div.appendChild(c);
 
-    // Product name
     const n = document.createElement("div");
     n.className = "name";
     n.innerText = name;
     div.appendChild(n);
 
-    // Quantity
     const q = document.createElement("div");
     q.className = "qty";
     q.innerText = current + " units";
     div.appendChild(q);
 
-    // Barcode
     if (barcode) {
-
-        const svg = document.createElementNS(
-            "http://www.w3.org/2000/svg",
-            "svg"
-        );
-
-        svg.classList.add("barcode");
-
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.className.baseVal = "barcode";
+        svg.dataset.barcode = barcode;
         div.appendChild(svg);
-
-        JsBarcode(svg, barcode, {
-            format: "CODE128",
-            width: 2,
-            height: 42,
-            displayValue: true,
-            fontSize: 15,
-            margin: 0
-        });
-
     }
 
-    // Manufacturing Order
     const m = document.createElement("div");
     m.className = "mo";
     m.innerText = mo;
@@ -108,8 +91,24 @@ for (let i = 0; i < pallets; i++) {
     labels.appendChild(div);
 }
 
-window.onload = () => {
+// Ждем, пока ВСЕ QR будут готовы
+Promise.all(qrPromises).then(() => {
+
+    document.querySelectorAll(".barcode").forEach(svg => {
+
+        JsBarcode(svg, svg.dataset.barcode, {
+            format: "CODE128",
+            width: 2,
+            height: 42,
+            displayValue: true,
+            fontSize: 15,
+            margin: 0
+        });
+
+    });
+
     setTimeout(() => {
         window.print();
-    }, 700);
-};
+    }, 200);
+
+});
